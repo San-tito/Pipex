07d4a6188213b4ae880d42bd4ffcd76f21e13ab8
Santito <santit0@protonmail.com>
2024-02-22 21:27:24 +0100

    :hammer: HAM

--- src/findcmd.c
+++ src/findcmd.c
@@ -6,7 +6,7 @@
 /*   By: sguzman <sguzman@student.42barcelona.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/22 19:15:47 by sguzman           #+#    #+#             */
-/*   Updated: 2024/02/22 20:08:28 by sguzman          ###   ########.fr       */
+/*   Updated: 2024/02/22 21:14:59 by sguzman          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,15 +35,48 @@ static char	*ft_getenv(const char *name, char **env)
 	return (NULL);
 }
 
+static char	*find_in_path_element(const char *name, char *path)
+{
+	char	*full_path;
+	size_t	path_len;
+	size_t	name_len;
+
+	path_len = ft_strlen(path);
+	name_len = ft_strlen(name);
+	full_path = ft_calloc(path_len + name_len + 2, sizeof(char));
+	if (!full_path)
+		return (NULL);
+	ft_strlcpy(full_path, path, path_len + 1);
+	if (*(full_path + path_len - 1) != '/')
+		ft_strlcat(full_path, "/", path_len + 2);
+	ft_strlcat(full_path, name, path_len + name_len + 2);
+	if (access(full_path, X_OK) == 0)
+		return (full_path);
+	free(full_path);
+	return (NULL);
+}
+
 char	*search_for_command(const char *pathname, char **env)
 {
 	char	*command;
 	char	*path_list;
+	char	**paths;
 
 	path_list = ft_getenv("PATH", env);
 	if (absolute_program(pathname))
 		command = ft_strdup(pathname);
-	if (!path_list || !*path_list)
+	else if (!path_list || !*path_list)
 		command = ft_strdup(pathname);
+	else
+	{
+		paths = ft_split(path_list, ':');
+		while (paths)
+		{
+			command = find_in_path_element(pathname, *paths);
+			if (command)
+				break ;
+			paths++;
+		}
+	}
 	return (command);
 }
--- src/pipex.c
+++ src/pipex.c
@@ -6,7 +6,7 @@
 /*   By: sguzman <sguzman@student.42barcelona.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/22 15:56:46 by sguzman           #+#    #+#             */
-/*   Updated: 2024/02/22 19:58:43 by sguzman          ###   ########.fr       */
+/*   Updated: 2024/02/22 21:09:05 by sguzman          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,11 @@
 
 int	main(int argc, char **argv, char **env)
 {
+	char	*command;
+
 	// pid_t	pid;
 	if (argc < 5)
 		return (EXIT_SUCCESS);
-	search_for_command(argv[1], env);
-
-	return (shell_execve(argv[1], argv + 2, env));
+	command = search_for_command(argv[1], env);
+	return (shell_execve(command, argv + 2, env));
 }
